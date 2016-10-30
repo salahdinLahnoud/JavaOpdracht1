09101b45d60ec184316aaa1437ee70b9acad230c
Salahdin Lahnoud <salahdin.lahnoud@gmail.com>
2016-10-30 21:53:57 +0100

    method commit

--- src/opdracht1/TestGitHUb.java
+++ src/opdracht1/TestGitHUb.java
@@ -3,7 +3,13 @@ package opdracht1;
 public class TestGitHUb {
 	public static void main(String [] args){
 		System.out.println("Test Git");
+		Succes();
 		
 	}
 
+	
+	public static void Succes(){
+		System.out.println("Sucses Test Git");
+		
+	}
 }
